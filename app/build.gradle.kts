bb28c3395ae567caeeebfaea8832257ef59b2987
mosayeb <angashtemosayeb@gmail.com>
2025-01-22 22:56:23 +0000

    update compile & target sdk

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,12 +13,12 @@ plugins {
 
 android {
     namespace = "com.ma.tehro"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.ma.tehro"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
