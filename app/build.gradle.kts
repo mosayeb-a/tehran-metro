0d814815542b53ca2dd962a421fbeba7fc13817f
mosayeb <angashtemosayeb@gmail.com>
2025-01-29 22:32:00 +0000

    update app version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
         applicationId = "com.ma.tehro"
         minSdk = 24
         targetSdk = 35
-        versionCode = 2
-        versionName = "0.1.0"
+        versionCode = 3
+        versionName = "0.2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
