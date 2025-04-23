298ac7882e99cd2c5ce0f74e7fabaf3131795460
mosayeb <angashtemosayeb@gmail.com>
2025-04-24 00:51:36 +0100

    refactor(colors): improve contrast and visibility for better accessibility

--- app/src/main/java/com/ma/tehro/common/Constants.kt
+++ app/src/main/java/com/ma/tehro/common/Constants.kt
@@ -1,10 +1,10 @@
 package com.ma.tehro.common
 
 
-const val COLOR_LINE_1 = "#FFDB4E49"
-const val COLOR_LINE_2 = "#FF2866A6"
-const val COLOR_LINE_3 = "#FF73B1E8"
-const val COLOR_LINE_4 = "#FFF4B653"
+const val COLOR_LINE_1 = "#FFD3443F"
+const val COLOR_LINE_2 = "#FF2D69A8"
+const val COLOR_LINE_3 = "#FF4EABCD"
+const val COLOR_LINE_4 = "#FFF0A634"
 const val COLOR_LINE_5 = "#FF49977C"
 const val COLOR_LINE_6 = "#FFE8629F"
 const val COLOR_LINE_7 = "#FF994CA1"
