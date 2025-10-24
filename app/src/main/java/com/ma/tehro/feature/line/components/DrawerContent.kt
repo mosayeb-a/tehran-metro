cfa890a19ae22d2ae1a80e9bf9351542d8d60f01
mosayeb <angashtemosayeb@gmail.com>
2025-10-24 21:10:55 +0100

    feat: add app version info to drawer content

--- app/src/main/java/com/ma/tehro/feature/line/components/DrawerContent.kt
+++ app/src/main/java/com/ma/tehro/feature/line/components/DrawerContent.kt
@@ -1,21 +1,19 @@
 package com.ma.tehro.feature.line.components
 
-import androidx.annotation.DrawableRes
 import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.WindowInsetsSides
+import androidx.compose.foundation.layout.asPaddingValues
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.navigationBars
 import androidx.compose.foundation.layout.only
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.systemBars
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.icons.Icons
@@ -25,18 +23,18 @@ import androidx.compose.material.icons.rounded.FormatListNumbered
 import androidx.compose.material.icons.rounded.Map
 import androidx.compose.material.icons.rounded.MyLocation
 import androidx.compose.material3.HorizontalDivider
-import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.vector.ImageVector
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.platform.LocalLayoutDirection
+import androidx.compose.ui.unit.LayoutDirection
 import androidx.compose.ui.unit.dp
+import com.ma.tehro.BuildConfig
 import com.ma.tehro.R
-import com.ma.tehro.common.ui.BilingualText
+import com.ma.tehro.common.toFarsiNumber
 import com.ma.tehro.data.BilingualName
 
 @Composable
@@ -48,118 +46,88 @@ fun DrawerContent(
     onMetroMapClick: () -> Unit,
     onMoreClick: () -> Unit
 ) {
-    LazyColumn(
+    Column(
         modifier = Modifier
             .fillMaxWidth(.71f)
             .fillMaxHeight()
             .background(MaterialTheme.colorScheme.secondary)
             .windowInsetsPadding(WindowInsets.systemBars.only(WindowInsetsSides.Top))
     ) {
-        item { Spacer(Modifier.height(56.dp)) }
-
-        item("lines") {
-            DrawerItem(
-                label = BilingualName(
-                    fa = "فهرست خطوط",
-                    en = "LINES"
-                ),
-                onClick = onLinesClick,
-                imageVector = Icons.Rounded.FormatListNumbered
-            )
-        }
-        item("pathfinder") {
-            DrawerItem(
-                label = BilingualName(
-                    fa = "مسیریابی",
-                    en = "PATHFINDER"
-                ),
-                onClick = onPathFinderClick,
-                icon = R.drawable.route
-            )
-        }
-        item("station_on_city_map") {
-            DrawerItem(
-                label = BilingualName(
-                    fa = "ایستگاها در نقشه شهر",
-                    en = "STATION ON CITY MAP"
-                ),
-                onClick = onCityMapClick,
-                imageVector = Icons.Rounded.MyLocation,
-            )
-        }
-        item("metro_map") {
-            DrawerItem(
-                label = BilingualName(
-                    fa = "نقشه مترو",
-                    en = "METRO MAP"
-                ),
-                onClick = onMetroMapClick,
-                imageVector = Icons.Rounded.Map,
-            )
-        }
-        item("submit_feedback") {
-            DrawerItem(
-                label = BilingualName(
-                    fa = "ارسال پیشنهاد",
-                    en = "SUBMIT FEEDBACK"
-                ),
-                onClick = onSubmitFeedbackClick,
-                imageVector = Icons.AutoMirrored.Rounded.Send
-            )
-        }
-        item("more") {
-            DrawerItem(
-                label = BilingualName(
-                    fa = "بیشتر",
-                    en = "MORE"
-                ),
-                onClick = onMoreClick,
-                imageVector = Icons.Default.HdrStrong
-            )
-        }
-    }
-}
-
-@Composable
-fun DrawerItem(
-    label: BilingualName,
-    onClick: () -> Unit,
-    @DrawableRes icon: Int? = null,
-    imageVector: ImageVector? = null
-) {
-    Column {
-        Row(
-            modifier = Modifier
-                .fillMaxWidth()
-                .clickable { onClick() }
-                .padding(end = 16.dp, top = 12.dp, bottom = 12.dp),
-            verticalAlignment = Alignment.CenterVertically,
-            horizontalArrangement = Arrangement.End
+        LazyColumn(
+            modifier = Modifier.weight(1f)
         ) {
-            BilingualText(
-                fa = label.fa,
-                en = label.en,
-                style = MaterialTheme.typography.bodyMedium,
-                maxLine = 2,
-                textAlign = TextAlign.End
-            )
+            item { Spacer(Modifier.height(56.dp)) }
 
-            Spacer(Modifier.width(16.dp))
-
-            if (icon != null) {
-                Icon(
-                    painter = painterResource(icon),
-                    contentDescription = label.fa,
-                    tint = Color.White
+            item("lines") {
+                DrawerItem(
+                    BilingualName("فهرست خطوط", "LINES"),
+                    onLinesClick,
+                    imageVector = Icons.Rounded.FormatListNumbered
                 )
-            } else if (imageVector != null) {
-                Icon(
-                    imageVector = imageVector,
-                    contentDescription = label.fa,
-                    tint = Color.White
+            }
+            item("pathfinder") {
+                DrawerItem(
+                    BilingualName("مسیریابی", "PATHFINDER"),
+                    onPathFinderClick,
+                    icon = R.drawable.route
+                )
+            }
+            item("station_on_city_map") {
+                DrawerItem(
+                    BilingualName(
+                        "ایستگاها در نقشه شهر",
+                        "STATION ON CITY MAP"
+                    ), onCityMapClick, imageVector = Icons.Rounded.MyLocation
+                )
+            }
+            item("metro_map") {
+                DrawerItem(
+                    BilingualName("نقشه مترو", "METRO MAP"),
+                    onMetroMapClick,
+                    imageVector = Icons.Rounded.Map
+                )
+            }
+            item("submit_feedback") {
+                DrawerItem(
+                    BilingualName("ارسال پیشنهاد", "SUBMIT FEEDBACK"),
+                    onSubmitFeedbackClick,
+                    imageVector = Icons.AutoMirrored.Rounded.Send
+                )
+            }
+            item("more") {
+                DrawerItem(
+                    BilingualName("بیشتر", "MORE"),
+                    onMoreClick,
+                    imageVector = Icons.Default.HdrStrong
                 )
             }
         }
-        HorizontalDivider(color = MaterialTheme.colorScheme.onBackground.copy(.3f))
+
+        HorizontalDivider(color = MaterialTheme.colorScheme.onBackground.copy(.06f))
+
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(MaterialTheme.colorScheme.primaryContainer.copy(alpha = 0.23f))
+                .padding(
+                    bottom = WindowInsets.navigationBars.asPaddingValues().calculateBottomPadding()
+                )
+                .padding(vertical = 16.dp),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(
+                text = "آخرین به‌روزرسانی",
+                style = MaterialTheme.typography.bodySmall,
+                color = MaterialTheme.colorScheme.onBackground.copy(alpha = 0.5f)
+            )
+            CompositionLocalProvider(LocalLayoutDirection provides LayoutDirection.Rtl) {
+                Text(
+                    text = "۲ آبان ۱۴۰۴ - نسخه ${BuildConfig.VERSION_NAME.toFarsiNumber()}",
+                    style = MaterialTheme.typography.bodySmall,
+                    color = MaterialTheme.colorScheme.onBackground.copy(alpha = 0.5f)
+                )
+            }
+        }
     }
 }
---
+++ app/src/main/java/com/ma/tehro/feature/line/components/DrawerItem.kt
@@ -0,0 +1,84 @@
+package com.ma.tehro.feature.line.components
+import androidx.annotation.DrawableRes
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.WindowInsetsSides
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.only
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.systemBars
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.windowInsetsPadding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.rounded.Send
+import androidx.compose.material.icons.filled.HdrStrong
+import androidx.compose.material.icons.rounded.FormatListNumbered
+import androidx.compose.material.icons.rounded.Map
+import androidx.compose.material.icons.rounded.MyLocation
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import com.ma.tehro.R
+import com.ma.tehro.common.ui.BilingualText
+import com.ma.tehro.data.BilingualName
+
+
+@Composable
+fun DrawerItem(
+    label: BilingualName,
+    onClick: () -> Unit,
+    @DrawableRes icon: Int? = null,
+    imageVector: ImageVector? = null
+) {
+    Column {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .clickable { onClick() }
+                .padding(end = 16.dp, top = 12.dp, bottom = 12.dp),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.End
+        ) {
+            BilingualText(
+                fa = label.fa,
+                en = label.en,
+                style = MaterialTheme.typography.bodyMedium,
+                maxLine = 2,
+                textAlign = TextAlign.End
+            )
+
+            Spacer(Modifier.width(16.dp))
+
+            if (icon != null) {
+                Icon(
+                    painter = painterResource(icon),
+                    contentDescription = label.fa,
+                    tint = Color.White
+                )
+            } else if (imageVector != null) {
+                Icon(
+                    imageVector = imageVector,
+                    contentDescription = label.fa,
+                    tint = Color.White
+                )
+            }
+        }
+        HorizontalDivider(color = MaterialTheme.colorScheme.onBackground.copy(.15f))
+    }
+}
